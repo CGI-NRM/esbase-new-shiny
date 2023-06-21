8db1062a196b3053c1c7fa973750bbbf59a75440
Elias Lundell <elilun03@gmail.com>
2023-06-21 19:19:02 +0200

    Generate tables for report

--- R/report.R
+++ R/report.R
@@ -2,12 +2,15 @@
 # But it is easier to create here, and to move later
 # Mainly so that all development will be trcaken in the
 # branch connected to the issue #11
-report_content <- function(file) {
+report_content <- function(file, biologdata, provlista) {
   # Place in tempdir since we don't have write access when deployed
   temp_report <- file.path(tempdir(), "report.Rmd")
   file.copy("report.Rmd", temp_report, overwrite = TRUE)
 
-  params <- list(n = 10)
+  params <- list(
+    biologdata = biologdata,
+    provlista = provlista
+  )
 
   # Knit the document, passing in the `params` list, and eval it in a child of the gloabl environment
   # This isolates the code in the document from the code in this app.
