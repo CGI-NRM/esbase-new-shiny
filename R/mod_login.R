d3fb29bf194a09e4818f834d4a7c9d8f72410f72
Elias Lundell <elilun03@gmail.com>
2023-08-08 17:34:56 +0200

    Login with enter on passwd

--- R/mod_login.R
+++ R/mod_login.R
@@ -34,9 +34,24 @@ mod_login_ui <- function(id) {
 }
 
 mod_login_server <- function(id, db, account) {
+  # A holder for wether to the user has logged in to stop it from logging in multiple times
+  status <- dataHolder(
+    logged_in = FALSE
+  )
+
   shiny::moduleServer(id, function(input, output, session) {
+
+    js_login_on_enter <- paste0('
+                                $(document).keyup(function(event) {
+                                  if ($("#', session$ns("passwd"), '").is(":focus") && (event.keyCode == 13)) {
+                                    $("#', session$ns("login"), '").click();
+                                  }
+                                });
+                                ')
+
     loginpage <- shiny::div(id = "loginpage", style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
                             shiny::wellPanel(
+                              shiny::tags$script(shiny::HTML(js_login_on_enter)),
                               shiny::tags$h2("Logga in", class = "text-center", style = "padding-top: 0;color:#333; font-weight:600;"),
                               shiny::textInput(session$ns("user_name"), placeholder = "Användarnamn",
                                                label = shiny::tagList(shiny::icon("user"), "Användarnamn")),
@@ -73,6 +88,7 @@ font-size: 18px; font-weight: 600;"),
         file.remove(paste0("active_sessions/", input$jscookie))
       }
       shinyjs::js$rmcookie()
+      status$logged_in <- FALSE
       shinyjs::refresh()
       logfine("mod_login.R - observeEvent(input$logout, {}): finished")
     })
@@ -80,15 +96,24 @@ font-size: 18px; font-weight: 600;"),
     shiny::observeEvent(input$jscookie, {
       logdebug("mod_login.R - observeEvent(input$jscookie, {}): called")
       logfiner(paste0("mod_login.R - observeEvent(input$jscookie, {}): login attempt with session uuid: ", input$jscookie))
-      if (!is.null(input$jscookie) && uuid::UUIDvalidate(input$jscookie) && file.exists(paste0("active_sessions/", input$jscookie))) {
-        account$id <- readLines(paste0("active_sessions/", input$jscookie))
+      if (!is.null(input$jscookie) &&
+          uuid::UUIDvalidate(input$jscookie) &&
+          file.exists(paste0("active_sessions/", input$jscookie))) {
+        if (status$logged_in) {
+          return()
+        }
+        account$id <- as.numeric(readLines(paste0("active_sessions/", input$jscookie)))
         logfiner(paste0("mod_login.R - observeEvent(input$jscookie, {}): logged in to user with id: ", account$id))
+        status$logged_in <- TRUE
         render_provberedning()
       } else {
         output$page <- shiny::renderUI(loginpage)
 
         shiny::observeEvent(input$login, {
           logdebug("mod_login.R - observeEvent(input$login, {}): called")
+          if (status$logged_in) {
+            return()
+          }
           if (!is.null(input$user_name) && !is.null(input$passwd)) {
             user <- db$person |> filter(username == input$user_name & password == input$passwd)
 
@@ -99,14 +124,18 @@ font-size: 18px; font-weight: 600;"),
                 uuid_session <- uuid::UUIDgenerate(use.time = FALSE, n = 1L, output = "string")
                 writeLines(as.character(account$id), paste0("active_sessions/", uuid_session))
 
-                shinyjs::js$setcookie(uuid_session)
-                logfiner(
-                  paste0(
-                    "mod_login.R - observeEvent(input$login, {}): logged in to user with id: ",
-                    account$id,
-                    ", using session: ",
-                    uuid_session))
-                render_provberedning()
+                  shinyjs::js$setcookie(uuid_session)
+                  logfiner(
+                    paste0(
+                      "mod_login.R - observeEvent(input$login, {}): logged in to user with id: ",
+                      account$id,
+                      ", using session: ",
+                      uuid_session))
+                  render_provberedning()
+                } else {
+                  shinyjs::show(id = "lowpermisions", anim = TRUE, time = 1, animType = "fade")
+                  shinyjs::delay(3000, shinyjs::hide(id = "lowpermisions", anim = TRUE, time = 1, animType = "fade"))
+                }
               } else {
                 logfiner("mod_login.R - observeEvent(input$login, {}): account$id not numeric, cannot login")
                 shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade")
